f68b2eee682ce0831824cf002292338a11e310ee
geo1976 <bobescugeorge@yahoo.com>
2020-05-22 15:54:46 +0100

    updating with the collection-item component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component'
 
 import './collection-preview.styles.scss';
 
@@ -9,9 +10,9 @@ const CollectionPreview = ({title,items}) => (
 <div className='preview'>
 {items
     .filter((item,idx)=>idx<4)
-    .map(item=>(
-        <div key={item.id}>{item.name}</div>
-        ))}
+    .map(({id, ...otheritemProps})=>(
+        <CollectionItem key={id} {...otheritemProps}/>
+    ))}
 </div>
 </div>
 
